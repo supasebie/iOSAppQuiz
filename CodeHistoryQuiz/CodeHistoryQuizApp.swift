ac5022de4f1b4a1012811862f673455dfd4a8d45
Sebastian <supasebie@gmail.com>
2024-05-15 20:00:59 -0700

    Initial Commit

---
+++ CodeHistoryQuiz/CodeHistoryQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryQuizApp.swift
+//  CodeHistoryQuiz
+//
+//  Created by Sebastian on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
